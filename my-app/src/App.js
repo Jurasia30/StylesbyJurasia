d4e74aafb219c019cbb91fe892a66ddf102142d6
Jurasia <jurasiabritton@gmail.com>
2023-05-23 18:02:01 -0400

    new project

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
+import Projects from './components/Projects';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Contact/>
+      <About/>
+      <Projects/>
+      
+    </div>
+  );
+}
+
+export default App;
